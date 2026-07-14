53ff28e79508a538ac76bce8e40a5354106b4d7c
PraveenK1618 <pyuish38@gmail.com>
2026-07-14 19:06:46 +0530

    Add SEO metadata sitemap RSS and favicon

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,30 @@
-import { getCollection } from 'astro:content';
-import rss from '@astrojs/rss';
-import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
-	});
+  const posts = await getCollection("blog", ({ data }) => {
+    return data.draft !== true;
+  });
+
+  const sortedPosts = posts.sort(
+    (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf(),
+  );
+
+  return rss({
+    title: "Praveen Builds",
+    description:
+      "Practical articles about software, electronics, embedded systems, automation and product building.",
+    site: context.site,
+
+    items: sortedPosts.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.pubDate,
+      link: `/blog/${post.id}/`,
+      author: post.data.author,
+      categories: [post.data.category],
+    })),
+
+    customData: `<language>en-in</language>`,
+  });
 }
